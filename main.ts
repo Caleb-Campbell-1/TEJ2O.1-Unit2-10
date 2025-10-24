54a5397e374c70fe83e9cb5ff6c5b1b3f1ad1af3
Caleb-Campbell-1 <caleb.campbell@stu.ocsb.ca>
2025-10-24 16:04:44 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,58 +5,57 @@
  * This program shows an if statement
 */
 
-// Clear Screen
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 // variables
-let Reading = 0
+let lightLevel = 0
 let neopixelStrip: neopixel.Strip = null
 
+// comment
+basic.forever(function () {
+    let neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    let lightLevel = input.lightLevel()
+    led.plotBarGraph(
+        lightLevel,
+        255
+    )
 
+    input.onButtonPressed(Button.A, function () {
+        // zero out all colors
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.showIcon(IconNames.Happy)
 
-    neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-    Reading = input.lightLevel()
-    led.plotBarGraph(Reading, 255)
-
-input.onButtonPressed(Button.A, function () {
-// Zero out all colors
-neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.show()
-basic.showIcon(IconNames.Happy)
-
-// select # of neopixels based on light level
-if (Reading > 208) {
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Yellow))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Orange))
-    neopixelStrip.show()
-    basic.pause(1000)
-    basic.showIcon(IconNames.Yes)
-} 
-if (Reading > 156) {
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Yellow))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
-    basic.pause(1000)
-    basic.showIcon(IconNames.Yes)
-} 
-if (Reading > 104) {
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
-    basic.pause(1000)
-    basic.showIcon(IconNames.Yes)
-}
-if (Reading > 52) {
+        // select # of neopixels based on light level
+        if (lightLevel > 208) {
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Yellow))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Orange))
+            neopixelStrip.show()
+            basic.pause(1000)
+            basic.showIcon(IconNames.Yes)
+        } else if (lightLevel > 156) {
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Yellow))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.show()
+            basic.pause(1000)
+            basic.showIcon(IconNames.Yes)
+        } else if (lightLevel > 104) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.pause(1000)
+        basic.showIcon(IconNames.Yes)
+    } else if (lightLevel > 52) {
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
@@ -64,12 +63,13 @@ if (Reading > 52) {
     neopixelStrip.show()
     basic.pause(1000)
     basic.showIcon(IconNames.Yes)
-} 
-if (Reading <= 51) {
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-        neopixelStrip.show()
+} else {
+    (lightLevel <= 51)
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
 }
+    })
 })
